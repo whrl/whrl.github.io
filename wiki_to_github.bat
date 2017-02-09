fd1897772bb94eff78f7e680ac0dce72f7b8f5ba
unknown <toni.woehrl@gmail.com>
2017-02-09 16:20:32 +0800

    Note 20170902_162032

---
+++ wiki_to_github.bat
@@ -0,0 +1,6 @@
+set datetimef=%date:~-4,4%%date:~-7,2%%date:~-10,2%_%time:~-11,2%%time:~-8,2%%time:~-5,2%
+
+
+set gitdir=C:\Users\tw\app\win\PortableGit\cmd\git.exe
+
+%gitdir%  add --all & %gitdir% commit -m "Note %datetimef%" & %gitdir% push
