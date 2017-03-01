4dd6a6d08bee418a4948624abc805adf094b9adf
unknown <toni.woehrl@gmail.com>
2017-03-01 17:41:17 +0800

    Note 20170103_174116

--- wiki_to_github.bat
+++ wiki_to_github.bat
@@ -6,7 +6,7 @@ set datetimef=%date:~-4,4%%date:~-7,2%%date:~-10,2%_%time:~-11,2%%time:~-8,2%%ti
 set gitdir=C:\Users\tw\app\win\PortableGit\cmd\git.exe
 
 cd %~dp0
-call lib\matlab-functions\createHTMLTable.bat
+call lib\matlab_whrl\createHTMLTable.bat
 
 
 
