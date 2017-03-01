8efdd4a8afa8ce4cf5ac9f36ff63a1d2d68d8c80
unknown <toni.woehrl@gmail.com>
2017-03-01 17:14:38 +0800

    Note 20170103_171437

--- wiki_to_github.bat
+++ wiki_to_github.bat
@@ -1,3 +1,5 @@
+TITLE %GITHUB%
+
 set datetimef=%date:~-4,4%%date:~-7,2%%date:~-10,2%_%time:~-11,2%%time:~-8,2%%time:~-5,2%
 
 
@@ -5,5 +7,7 @@ set gitdir=C:\Users\tw\app\win\PortableGit\cmd\git.exe
 
 cd %~dp0
 call lib\matlab-functions\createHTMLTable.bat
+
+nircmd win activate %GITHUB%
 cd %~dp0
 %gitdir%  add --all & %gitdir% commit -m "Note %datetimef%" & %gitdir% push
