286f485f3d47c8a553e9ef27e553e7f03b054611
unknown <toni.woehrl@gmail.com>
2017-03-01 22:12:48 +0800

    Removed none html

--- lib/matlab_whrl/createHTMLTable.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0
-emacs --script whrl_create.el
-C:\Program Files (x86)\Google\Chrome\Application\chrome index.html
--- readme_themes.txt
+++
@@ -1,2 +0,0 @@
-theme_v2.css - Sophisticated left navigation bar for TOC
-theme_v1.css - Org mode css
