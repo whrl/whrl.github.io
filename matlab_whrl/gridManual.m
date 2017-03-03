9b26419462724ee9c11bc2fee4a759787bc2b66d
unknown <toni.woehrl@gmail.com>
2017-03-03 14:21:50 +0800

    Note 20170303_142150

---
+++ matlab_whrl/gridManual.m
@@ -0,0 +1,37 @@
+function gridManual(steps,style)
+% info: plot grid with different spacings
+%
+% syntax: gridManual(steps,style)
+%
+% input: 
+%
+% output: 
+%
+% example: gridManual([-20:5:20],{'Linestyle',':','Color',[1 1 1]*.5})
+%
+% requires: 
+%
+% see also: 
+%
+% created with: MATLAB ver. 8.6.0.267246 (R2015b)
+% created by: T. Woehrl
+% created on: 2015-07-31
+%
+% modified:
+% - on:  by:  to:
+%
+% disclaimer: Without warranty of any kind.
+%
+%------------- BEGIN CODE ---------------
+grid_n = length(steps);
+
+lines1 = [steps;steps];
+
+lines2 = ones(2,grid_n);
+lines2(1,1:grid_n) = ones(1,grid_n).*steps(1);
+lines2(2,1:grid_n) = ones(1,grid_n).*steps(end);
+
+plot(lines1,lines2,style{:}); hold on
+plot(lines2,lines1,style{:})
+end
+%------------- END OF CODE --------------
